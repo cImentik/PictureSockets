3831f775eef9bc080b4a976b4f46779e1114942d
cImentik <ivan.susanin.molodez@gmail.com>
2015-10-05 23:39:23 +0700

    Реализация потоков и сокетов

---
+++ WindowsFormsApplicationPicture/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApplicationPicture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundLoadFileWorker = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.DialogResult = System.Windows.Forms.DialogResult.Abort;
+            this.downloadButton.Location = new System.Drawing.Point(229, 428);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 0;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(486, 316);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 355);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "не готово";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "не готово";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(200, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "не готово";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(200, 394);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "не готово (задержка 2000мс)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 463);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.downloadButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundLoadFileWorker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplicationPicture/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+/* *
+ * Реализовать передачу данных средствами набора сокетов поочередно друг другу. 
+ * Первый сокет на входе получает картинку и отсылает ее следующему, который в свою очередь следующему и так далее. 
+ * Последний сокет получив картинку выводит ее на форму. Количество сокетов не менее 4.
+ * Данные должны передавать непрерывно и не вызывать зависания формы.
+ * */
+namespace WindowsFormsApplicationPicture
+{
+    public partial class Form1 : Form
+    {
+        const int threadCount = 4; //Количество потоков с сокетами
+             
+        private String g_host = "127.0.0.1"; //начальный хост (он же единственный)
+        private int g_port = 50000; //начальный порт (далее увеличиваем на 1 для каждого сокета)
+        private FileStream fs; //файловый поток для загружаемого файла
+        private Image img; // объект для картинки
+
+
+        //массив с необходимым количеством воркеров в очереди
+        private BackgroundWorker[] backgroundWorkerSockets = new BackgroundWorker[threadCount];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeBackgroundWorker(); 
+        }
+
+        /// <summary>
+        /// Инициализация воркеров. 
+        /// Отделный воркер для загрузки файла и перебор с настройкой массива "рабочих" воркеров
+        /// </summary>
+        private void InitializeBackgroundWorker()
+        {
+            backgroundLoadFileWorker.DoWork += new DoWorkEventHandler(
+                backgroundLoadFileWorker_DoWork);
+            backgroundLoadFileWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(
+                backgroundLoadFileWorker_RunWorkerCompleted);
+
+            for (int i = 0; i < backgroundWorkerSockets.Length; i++)
+            {
+                backgroundWorkerSockets[i] = new BackgroundWorker();
+                backgroundWorkerSockets[i].DoWork += new DoWorkEventHandler(
+                    this.DoWork);
+                backgroundWorkerSockets[i].RunWorkerCompleted += new RunWorkerCompletedEventHandler(
+                    this.RunWorkerCompleted);
+                backgroundWorkerSockets[i].RunWorkerAsync(i + 1); //сразу слушают порты и ждут файла
+            }
+        }
+
+        /// <summary>
+        /// Один общий DoWork для всех "рабочих" воркеров. В качестве параметра ждёт номер потока
+        /// По номеру будет создан нужный порт.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            int threadSocketNumber = (int)e.Argument;
+            this.WorkerSocket(threadSocketNumber);
+            e.Result = threadSocketNumber; //транслируем дальше в Completed
+        }
+
+        /// <summary>
+        /// Один общий RunWorkerCompleted для всех "рабочих" воркеров.В качестве параметра ждёт номер потока.
+        /// По номеру потоку будет информировать (на форме) об успехе каждого сокета или о неудачи.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            int numberOfSocket = (int)e.Result;
+            if (e.Error == null)
+            {
+                Controls["label" + numberOfSocket.ToString()].Text = "Сокет" + numberOfSocket + " - готово";
+                if (numberOfSocket == threadCount) //Если это последний сокет из очереди
+                    pictureBox1.Image = img;     //то выводим на форму
+            }
+            else
+            {
+                this.ShowErrorMessage(numberOfSocket);
+            }
+        }
+
+
+        /// <summary>
+        /// Обработчик кнопки, который стартует поток на загрузку файла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    this.backgroundLoadFileWorker.RunWorkerAsync();
+                    this.downloadButton.Enabled = false;
+
+                    while (this.backgroundLoadFileWorker.IsBusy)//поток жив 
+                    {
+                        Application.DoEvents(); 
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Operation failed",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+            
+        }
+
+
+        /// <summary>
+        /// DoWork непосредственно выполняющий считывание указанного в диалоге открытия файл.
+        /// Также он отправляет файл в первый по очереди сокет
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void backgroundLoadFileWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try {
+                fs = new FileStream(openFileDialog1.FileName, FileMode.Open);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    fs.CopyTo(memoryStream);
+                    memoryStream.Position = 0;
+                    MyListener list = new MyListener();
+                    list.Sender(memoryStream, g_host, g_port);
+                }
+                //Thread.Sleep(1000); //чтобы успеть посмотреть
+                //fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "File operation failed",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+
+        /// <summary>
+        /// Если результат плачевный уведомляем об этом пользователя
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void backgroundLoadFileWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+               //MessageBox.Show("Download Complete");
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Failed to download file",
+                    "Download failed",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            this.downloadButton.Enabled = true;
+        }
+
+        private void ShowErrorMessage(int namberOfSocked)
+        {
+            MessageBox.Show("Failed to socket" + namberOfSocked.ToString(), "Connected failed",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Error);
+        }
+
+        /// <summary>
+        /// Метод работы с сокетами.
+        /// В каждом потоке созаётся экземпляр MyListener, который осуществляет приём и отправку полученного файла
+        /// </summary>
+        /// <param name="namberOfSocked"></param>
+        private void WorkerSocket(int namberOfSocked)
+        {
+            MyListener list = new MyListener();
+            list.Receiver(g_host, g_port + (namberOfSocked - 1)); //Слушаем и получаем файл если он есть
+            if (namberOfSocked < threadCount) //если не последний сокет
+            {
+                list.GetMemorystream().Position = 0;
+                //оправить полученый файл в следующий сокет
+                list.Sender(list.GetMemorystream(), g_host, g_port + namberOfSocked);  
+            }
+            else
+            { //если это последний сокет ждём и показываем на форме
+                Thread.Sleep(2000);
+                img = System.Drawing.Image.FromStream(list.GetMemorystream());
+            }
+        }
+    }
+
+    /// <summary>
+    /// Вспомогательный класс для логики работы сокета. Обладает в себе возможностью отправки и приёма
+    /// файла.
+    /// </summary>
+    public class MyListener
+    {
+        private MemoryStream memorystream; //поток для хранения файла между отпракой и посылкой
+
+        public MyListener()
+        {
+            memorystream = new MemoryStream();
+        }
+
+        public MemoryStream GetMemorystream()
+        {
+            return memorystream;
+        }
+
+        /// <summary>
+        /// Создание соеденения и отправка файла. Создаётся буфер и файл отправляетя по кускам, читаемым из потока,
+        /// эквивалентным размеру буфера
+        /// </summary>
+        public void Sender(MemoryStream memorystream, String host, int port)
+        {
+            try
+            {
+                IPEndPoint EndPoint = new IPEndPoint(IPAddress.Parse(host), port);
+                Socket Connector = new Socket(EndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                Connector.Connect(EndPoint);
+                Byte[] ReadedBytes = new Byte[256];
+
+                using (var Reader = new BinaryReader(memorystream, Encoding.UTF8, true))
+                {
+                    Int32 CurrentReadedBytesCount;
+                    do
+                    {
+                        CurrentReadedBytesCount = Reader.Read(ReadedBytes, 0, ReadedBytes.Length);
+                        Connector.Send(ReadedBytes, CurrentReadedBytesCount, SocketFlags.None);
+                    }
+                    while (CurrentReadedBytesCount == ReadedBytes.Length);
+                }
+                Connector.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Connected failed",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+            
+        }
+
+        /// <summary>
+        /// Ожтдает отправки файла. Если файл получен уложить его в this.memorystream,
+        /// чтобы потом утправить методом Sender
+        /// </summary>
+        public void Receiver(String host, int port)
+        {
+            try
+            {
+                MemoryStream mStream = new MemoryStream();
+                TcpListener Listen = new TcpListener(IPAddress.Parse(host), port);
+                Listen.Start();
+                Socket ReceiveSocket;
+                while (true)
+                {
+                    ReceiveSocket = Listen.AcceptSocket();
+                    Byte[] Receive = new Byte[256];
+                    using (MemoryStream MessageFile = new MemoryStream())
+                    {
+                        //Количество считанных байт
+                        Int32 ReceivedBytes;
+                        do
+                        {
+                            ReceivedBytes = ReceiveSocket.Receive(Receive, Receive.Length, 0);
+                            MessageFile.Write(Receive, 0, ReceivedBytes);
+                            //Читаем до тех пор, пока в очереди не останется данных
+                        } while (ReceivedBytes == Receive.Length);
+
+                        MessageFile.Position = 0;
+                        MessageFile.CopyTo(this.memorystream);
+
+                    }
+                    break; //не принципиально, но тогда можно явно закончить поток
+                    // и отобразить на форме
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Connected failed",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+
+        }
+
+    }
+}
